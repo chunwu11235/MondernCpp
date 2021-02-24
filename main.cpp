466f18e488d3c71a4b42e4e1aecd45ab1f890eb1
Chun Wu <n4rjk00@gmail.com>
2021-02-24 00:17:22 -0700

    cmake setup for  header files

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "my_lib.h"
+
+using namespace std;
+
+int main(int, char**) {
+    cout << "Hello, world!\n";
+    cout << my_add(1, 2) << endl;
+}
---
+++ src/my_lib.cpp
@@ -0,0 +1,5 @@
+#include "my_lib.h"
+
+int my_add(int a, int b) {
+    return a + b;
+}
