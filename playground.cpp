2b4451cff606d8f8128fcf571b1b48733bd4a86d
Chun Wu <n4rjk00@gmail.com>
2021-02-24 19:52:22 -0700

    set cmake to c++ 17

--- playground.cpp
+++ playground.cpp
@@ -7,4 +7,16 @@ using namespace std;
 int main(int, char**) {
     cout << "Hello, world!\n";
     printf("My addition: 1 + 2 = %d\n", my_add(1, 2) );
+
+    // uniform initialization'
+    int a{10};
+    printf("%d", a);
+
+    // int a_ref{&a};
+    // printf("%d", a_ref);
+
+    int *a_ptr{new int{10}};
+    cout<< a_ptr << "\t" << *a_ptr << endl;
+
+
 }
