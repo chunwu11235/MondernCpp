a94ce66a841fdd3f14631e381509af7ce485c337
Chun Wu <n4rjk00@gmail.com>
2021-02-24 21:03:07 -0700

    pointer reference and uniform initialization

--- playground.cpp
+++ playground.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "my_lib.h"
 #include "stdio.h"
+#include "string"
+
 
 using namespace std;
 
@@ -10,13 +12,33 @@ int main(int, char**) {
 
     // uniform initialization'
     int a{10};
-    printf("%d", a);
+    cout << a << "\t" << &a << "\n";
 
-    // int a_ref{&a};
-    // printf("%d", a_ref);
+    int a_ref{a};
+    cout << a_ref << "\t" << &a_ref << "\n";
+    
+    int *a_ptr{&a};
+    cout << a_ptr << "\t" << *a_ptr << "\n";
 
-    int *a_ptr{new int{10}};
-    cout<< a_ptr << "\t" << *a_ptr << endl;
+    int *a_ptr2 = nullptr; // always use nullptr;
+    // *a_ptr2 = 100; // cannot read and write a nullptr
+    a_ptr2 = &a; // need to assign address first
+    *a_ptr2 = 5;
+    cout << a << "\n";
 
+    cout << *a_ptr2 << "\n";
 
+    *a_ptr = 5; // same as &a = 5, same as a = 5
+    cout << a << "\n";
+
+    int arr[]{1,2,3,4,5};
+    cout << arr << "\t" << arr[2] << "\n";
+    int *arr_ptr = arr;
+    cout << arr_ptr << "\t" << arr_ptr[3] << "\n";
+
+    // void pointer
+    void *ptr = &a;
+
+    char *c_ptr = new char{'c'};
+    cout << (void * ) &c_ptr << "\t" << *c_ptr << endl; // void * to print the address of char ptr
 }
