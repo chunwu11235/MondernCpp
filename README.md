f204df3a41fab45054b7e46fb2b5063a24f90531
Chun Wu <n4rjk00@gmail.com>
2021-04-03 22:36:41 -0600

    builder

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # ModernCpp
 
-Play around with modern c++
+Play Around With Modern C++
+1. Core language features of C++ 11/14/17
+   1. Built-in types
+   2. OOP
+   3. Move Sematic
+   4. Smart pointers
+   5. Lambda
+   6. Template
+   7. STL
+   8. CMake
+2. Design Patterns
+   1. Creational
+   2. Structural
+   3. Behavioral
--- design_patterns/builder/builder.cpp
+++ design_patterns/builder/builder.cpp
@@ -1,23 +1,70 @@
 #include "builder.h"
 #include <string>
 #include <iostream>
+#include <memory>
 
 using namespace std;
-
-class HouseBuilder;
 
 class House{
 private:
     int floors{0};
     double price{0};
     std::string address{};
-    std::string owner{};
-    House();
+    House() {};
+
+    void setFloors(const int floors) {
+        this->floors = floors;
+    }
+
+    void setPrices(const double price) {
+        this->price = price;
+    }
+
+    template<typename T>
+    void setAddress(T&& address) {
+        this->address = std::forward<T>(address);
+    }
+    
+public:
+    static class HouseBuilder {
+    private:
+        House* house{nullptr};
+    public:
+        HouseBuilder(): house{new House{}} {}
+        HouseBuilder& withFloors(int floors) {
+            house->setFloors(std::move(floors));
+            return *this;
+        }
+
+        HouseBuilder& withPrice(double price) {
+            house->setPrices(std::move(price));
+            return *this;
+        }
+
+        template<typename T>
+        HouseBuilder& withAddress(T&& address) {
+            house->setAddress(std::forward<T>(address));
+            return *this;
+        }
+
+        House build() {
+            return std::move(*house);
+        }
+    };
 
 public:
-    static HouseBuilder createBuilder();
+    static HouseBuilder createBuilder() {
+        return House::HouseBuilder{};
+    };
 };
 
 void demo_builder() {
     std::cout << "--- demo builder ---" << endl;
+
+    auto house1 = House::createBuilder()
+        .withAddress(std::string("123 A Street, AAA City, GG 88888"))
+        .withFloors(19)
+        .withPrice(15e5)
+        .build();
+
 }
