8b7b8950f799fd36883f00e563b4ce8c5eef5f56
Chun Wu <n4rjk00@gmail.com>
2021-04-05 18:13:21 -0600

    factory methods

--- design_patterns/factory/factory.cpp
+++ design_patterns/factory/factory.cpp
@@ -1,1 +1,36 @@
 #include "factory.h"
+#include "string"
+#include <iostream>
+
+struct Shape {
+    std::string type{};
+    double area{};
+    void draw() const {
+        std::cout << type << " with area " << area << std::endl;
+    };
+protected:
+    Shape(std::string type, double area): type{type}, area{area} {};
+};
+
+class Square: public Shape {
+    private:
+        Square(double area): Shape{"square", area} {};
+    public:
+        // factory methods
+        // they have the same parameters, so we are not able to overload constructors
+        static Square createFromArea(double area) {
+            return Square{area};
+        }
+
+        static Square createFromLength(double length) {
+            return Square{length * length};
+        }
+};
+
+void demo_factory() {
+    std::cout << "--- Factory Methods ---\n";
+    Square::createFromArea(10).draw();
+    Square::createFromLength(5).draw();
+}
+
+
