4cfa2cd92290efc54b2898fac44f2766a3398e04
Chun Wu <n4rjk00@gmail.com>
2021-04-11 13:03:40 -0600

    functional decorator: simple timer for void functions

--- design_patterns/decorator/functional_decorator.cpp
+++ design_patterns/decorator/functional_decorator.cpp
@@ -1,8 +1,33 @@
 #include "functional_decorator.h"
 #include <iostream>
+#include <chrono>
+#include <functional>
+
+template<typename Callable>
+struct SimpleTimer{
+    Callable callable;
+
+    SimpleTimer(Callable callable): callable{callable} {};
+    void operator() () {
+        auto start = std::chrono::steady_clock::now();
+        callable();
+        auto end = std::chrono::steady_clock::now();
+        std::cout << "Elapsed time: " << (end - start).count() << std::endl;
+    }    
+
+};
+
+
+void func1() {
+    std::cout<< "func1()\n";
+}
 
 void demo_functional_decorator() {
     std::cout << "--- Demo Functional Decorator---\n";
+
+    std::cout << "--- void function\n";
+    SimpleTimer<std::function<void()>> f1{func1};
+    f1();
 
     
 };
