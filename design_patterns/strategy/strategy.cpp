ad0764201fca7c5362d628a3fccd3000b74e9d9e
Chun Wu <n4rjk00@gmail.com>
2021-04-12 22:26:34 -0600

    dynamic strategy

--- design_patterns/strategy/strategy.cpp
+++ design_patterns/strategy/strategy.cpp
@@ -1,6 +1,75 @@
 #include "strategy.h"
 #include <iostream>
+#include <vector>
+#include "string.h"
+#include <memory>
+
+using namespace std;
+
+struct Book{
+    int total_page{};
+    int current_page{};
+
+    Book(int total_page): total_page{total_page}, current_page{0} {};
+    void move_current_page(int pages) {
+        if( current_page + pages < total_page ) {
+            current_page += pages;
+        }else{
+            current_page = total_page;
+        };
+    }
+};
+
+struct IStudyStrategy {
+    virtual void study(Book&) const = 0;
+};
+
+struct StudyAtHome : IStudyStrategy {
+    void study(Book& book) const override {
+        book.move_current_page(20);
+    }
+};
+
+struct StudyAtLibrary : IStudyStrategy {
+    void study(Book& book) const override {
+        book.move_current_page(50);
+    }
+};
+
+struct Undergrad{
+    string name{};
+    Book& book;
+    shared_ptr<IStudyStrategy> strategy;
+    Undergrad(string&& name, Book& book, const shared_ptr<IStudyStrategy>& strategy): 
+        name{std::move(name)}, book{book}, strategy{strategy} {};
+
+    void prepareForMidterm() const {
+        cout << name << " started from page " << book.current_page << endl;
+        strategy->study(book);
+        cout << name << " has read to page" << book.current_page << endl;
+    }
+
+    void setStrategy(const shared_ptr<IStudyStrategy>& strategy) {
+        this->strategy = strategy;
+    }
+};
+
+
 void demo_strategy() {
     std::cout << "--- demo strategy \n";
 
-}
+    std::cout << "--- dynamic strategy\n";
+
+    auto strategy1 = make_shared<StudyAtHome>();
+    auto strategy2 = make_shared<StudyAtLibrary>();
+    Book Math101{300};
+    Book CS101{500};
+
+    Undergrad Lisa{"Lisa", CS101, strategy1};
+    Lisa.prepareForMidterm();
+    Lisa.setStrategy(strategy2);
+    Lisa.prepareForMidterm();
+
+    
+
+};
