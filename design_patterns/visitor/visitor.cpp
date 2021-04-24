b266512a2e7336f28367dbcafa5ab3179ed2b59c
Chun Wu <n4rjk00@gmail.com>
2021-04-24 14:37:22 -0600

    visit

--- design_patterns/visitor/visitor.cpp
+++ design_patterns/visitor/visitor.cpp
@@ -1,8 +1,61 @@
 #include "visitor.hpp"
 #include <iostream>
 using namespace std;
+
+void Mountain::accept(Visitor* v) {
+    v -> visit(this);
+}
+
+void Lake::accept(Visitor* v) {
+    v -> visit(this);
+}
+
+void City::accept(Visitor* v) {
+    v -> visit(this);
+}
+
+void Visitor::visit(Location* location) {
+    cout << this->name << " is passing " << location->name << endl;
+    if(location->next != nullptr) {
+        location->next->accept(this);
+    }
+}
+
+void Hiker::visit(Mountain* location) {
+    cout << this->name << " is hiking at " << location->name << endl;
+    if(location->next != nullptr) {
+        location->next->accept(this);
+    }
+}
+
+void Foodie::visit(City* location) {
+    cout << this->name << " is eating at " << location->name << endl;
+    if(location->next != nullptr) {
+        location->next->accept(this);
+    }
+}
+
+
 void demo_visitor() {
     cout << "--- DEMO VISITOR --- \n";
+
+    auto path1 = City("Las Vegas",
+        make_shared<Mountain>("Grand Canyon", 
+        make_shared<City>("LA", 
+        make_shared<Lake>("Lake Tahoe", 
+        nullptr))));
+
+    Hiker hiker{"Anderson"};
+    Foodie foodie("Jason");
+    
+
+    path1.accept(&hiker);
+    cout << "---\n";
+    path1.accept(&foodie);
+
+
+
+
 
     cout << "--- PROGRAM END --- \n";
 }
--- design_patterns/visitor/visitor.hpp
+++ design_patterns/visitor/visitor.hpp
@@ -2,6 +2,59 @@
 #ifndef DESIGN_PATTERNS_VISITOR
 #define DESIGN_PATTERNS_VISITOR
 
+#include <string>
+#include <memory>
+#include <iostream>
+using namespace std;
+
+struct Visitor;
+
+struct Location {
+    virtual ~Location() {
+        cout << "~Location()\n"; // for debuggin
+    };
+    virtual void accept(Visitor* visitor) = 0;
+   
+    string name;
+    shared_ptr<Location> next;
+    Location(const string& name, const shared_ptr<Location> next): 
+        name{name}, next{next} {
+            cout << "Location()\n"; // for debuggin
+    };
+};
+
+struct Mountain : Location{
+    Mountain(const string& name, const shared_ptr<Location> next): Location(name, next) {};
+    void accept(Visitor* visitor) override;
+};
+
+struct City : Location {
+    City(const string& name, const shared_ptr<Location> next): Location(name, next) {};
+    void accept(Visitor* visitor) override;
+};
+
+struct Lake : Location {
+    Lake(const string& name, const shared_ptr<Location> next): Location{name, next} {};
+    void accept(Visitor* visitor) override;
+};
+
+struct Visitor {
+    string name;
+    Visitor(const string& name): name{name} {};
+    virtual void visit(Location* location);
+};
+
+struct Hiker : Visitor {
+    Hiker(const string& name): Visitor{name} {};
+    virtual void visit(Mountain*);
+}; 
+
+struct Foodie : Visitor {
+    Foodie(const string& name): Visitor{name} {};
+    virtual void visit(City*);
+};
+
+
 void demo_visitor();
 
 #endif
