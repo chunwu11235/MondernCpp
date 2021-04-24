7bcb5d270d3d18f0fd7854ce2c50cb46b00bef3e
Chun Wu <n4rjk00@gmail.com>
2021-04-23 21:47:37 -0600

    demo state with a  Monte-Carlo simulation of a dice game

--- design_patterns/state/state.cpp
+++ design_patterns/state/state.cpp
@@ -1,9 +1,66 @@
 #include "state.hpp"
 #include <iostream>
+#include <stdlib.h>
 
 using namespace std;
 
+DiceGame::DiceGame(): _state{new NoSix{}} {};
+
+void DiceGame::play() {
+    int toss = rand() % 6 + 1;
+    // cout << "Toss " << toss << endl;
+    _state->transit(*this, toss);
+}
+
+bool DiceGame::stop() const{
+    return _state->isEnd;
+}
+
+void NoSix::transit(DiceGame& dice_game, int trigger) {
+    if(trigger == 6) {
+        dice_game.set_state(new OneSix());
+        delete this;
+    }
+}
+
+void OneSix::transit(DiceGame& dice_game, int trigger) {
+    if(trigger == 6) {
+        dice_game.set_state(new TwoSix());
+    }else{
+        dice_game.set_state(new NoSix());
+    }
+    delete this;
+}
+
+TwoSix::TwoSix() {
+    isEnd = true;
+};
+
+void TwoSix::transit(DiceGame& dice_game, int trigger) {}
+
 void demo_state() {
     cout << "--- DEMO STATE ---\n";
+
+    int n_simu{500};
+    int count{0};
+
+    // Monte-Carlo simulation
+    for(int i = 0; i < n_simu; ++i) {
+        cout << "--- New Game ---\n";
+        DiceGame game{};
+        int _count{};
+        while(!game.stop()) {
+            game.play();
+            ++_count;
+        }
+        cout << _count << " toss" << endl;
+        cout << "--- End Game ---\n";
+        count += _count;
+
+    }
+
+    cout << (float) count/n_simu << endl; // should be close to 42
+
+
     cout << "--- DEMO END --- \n";
 }
--- design_patterns/state/state.hpp
+++ design_patterns/state/state.hpp
@@ -2,6 +2,39 @@
 #ifndef DESIGN_PATTERNS_STATE
 #define DESIGN_PATTERNS_STATE
 
+struct State;
+
+// keep tossing dice until you get 2 consecutive 6
+struct DiceGame{
+    State* _state{nullptr};
+    DiceGame();
+    void set_state(State* s) {
+        _state = s;
+    };
+    void play();
+    bool stop() const;
+};
+
+struct State{
+    bool isEnd{false};
+    virtual void transit(DiceGame& dice_game, int trigger) = 0;
+};
+
+struct NoSix : State {
+    virtual void transit(DiceGame& dice_game, int trigger) override;
+};
+
+struct OneSix : State {
+    virtual void transit(DiceGame& dice_game, int trigger) override;
+};
+
+struct TwoSix : State {
+    TwoSix();
+    virtual void transit(DiceGame& dice_game, int trigger) override;
+}; 
+
+
+
 void demo_state();
 
 #endif
